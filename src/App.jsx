a6e2e54c5c02ed48a1cd676fabf916480c2bb54e
ArticSaber <kaarthiknirmala@gmail.com>
2023-03-07 21:06:09 +0530

    pic insertion

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,48 @@ import "./App.css";
 import { BsTelephone } from "react-icons/bs";
 import { AiOutlineMail } from "react-icons/ai";
 import { GrHomeRounded } from "react-icons/gr";
+import photo from "./assets/image.jpeg";
 function App() {
   return (
-    <div className="maincontainer">
+    <div className="App">
       <div className="container">
         <hr className="line" size="10" />
-        <div className="heading">KAARTHIKEYAN S</div>
-        <div className="heading">B.E (CSE)</div>
+        <div className="heading">
+          {/* <img
+            src={photo}
+            alt="image"
+            className="image" /> */}
+          <div className="head">
+          <span>KAARTHIKEYAN S</span>
+            <span>B.E (CSE)</span>
+            </div>
+        </div>
         <hr className="line" size="10" />
         <div className="box">
           <div className="left">
             <div className="contact">
               CONTACT
-              <div className="phone contact-item">
-                <BsTelephone className="icon" /> 8072920907
+              <div className="contact-item">
+                <BsTelephone className="icon" />
+                8072920907
               </div>
-              <div className="mail contact-item">
-                <AiOutlineMail className="icon" /> kaarthiknirmala@gmail.com
+              <div className="contact-item">
+                <AiOutlineMail className="icon" />
+                kaarthiknirmala@gmail.com
               </div>
-              <div className="address contact-item">
+              <div className="contact-item">
                 <GrHomeRounded className="icon" /> No.114, Flat E, Chendur
                 Castle, Kattupakkam, Chennai – 600 056.
+              </div>
+              <div className="skills">
+                TECHNICAL SKILLS
+                <ul>
+                  <li>C</li>
+                  <li>Python</li>
+                  <li>HTML</li>
+                  <li>ReactJS</li>
+                  <li>CSS</li>
+                </ul>
               </div>
             </div>
           </div>
